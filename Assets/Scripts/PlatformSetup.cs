4a4c5a988487ff7bc41f7f0a8ccb57e869f192c8
Valentino Cavaricci <valentinocavaricci@Macbook-Pro-2.local>
2026-04-20 10:37:28 -0700

    added UI and deaht logic

--- Assets/Scripts/PlatformSetup.cs
+++ Assets/Scripts/PlatformSetup.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PlatformSetup : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private BoxCollider2D bc;
 
     public bool isRed;
+    public bool hasBeenScored = false;
+    public bool isDisappearing = false;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
 
         float randomWidth = Random.Range(2f, 4.5f);
         transform.localScale = new Vector3(randomWidth, transform.localScale.y, 1f);
@@ -23,5 +27,19 @@ public class PlatformSetup : MonoBehaviour
         {
             sr.color = Color.blue;
         }
+    }
+
+    public void StartDisappear()
+    {
+        if (!isDisappearing)
+        {
+            isDisappearing = true;
+            Invoke(nameof(Disappear), 0.25f);
+        }
+    }
+
+    void Disappear()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float jumpForce = 6f;
+    public float deathY = -6f;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverPanel;
+    public GameObject pausePanel;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
 
     private bool isRed = true;
     private bool isGameOver = false;
+    private bool isPaused = false;
+
+    private int score = 0;
 
     void Start()
     {
@@ -17,17 +25,36 @@ public class PlayerMovement : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
 
         Time.timeScale = 1f;
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+
         UpdateColor();
+        UpdateScoreUI();
     }
 
     void Update()
     {
         if (isGameOver)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                RestartGame();
-            }
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+
+        if (isPaused)
+            return;
+
+        if (transform.position.y < deathY)
+        {
+            GameOver();
             return;
         }
 
@@ -46,17 +73,22 @@ public class PlayerMovement : MonoBehaviour
     void UpdateColor()
     {
         if (isRed)
-        {
             sr.color = Color.red;
-        }
         else
-        {
             sr.color = Color.blue;
-        }
+    }
+
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (isGameOver || isPaused)
+            return;
+
         PlatformSetup platform = collision.gameObject.GetComponent<PlatformSetup>();
 
         if (platform != null)
@@ -64,18 +96,53 @@ public class PlayerMovement : MonoBehaviour
             if (platform.isRed != isRed)
             {
                 GameOver();
+                return;
+            }
+
+            if (!platform.hasBeenScored)
+            {
+                platform.hasBeenScored = true;
+                score++;
+                UpdateScoreUI();
+                platform.StartDisappear();
             }
         }
     }
 
     void GameOver()
     {
+        if (isGameOver)
+            return;
+
         isGameOver = true;
-        Debug.Log("Game Over - Press R to Restart");
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+
         Time.timeScale = 0f;
     }
 
-    void RestartGame()
+    void PauseGame()
+    {
+        isPaused = true;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
